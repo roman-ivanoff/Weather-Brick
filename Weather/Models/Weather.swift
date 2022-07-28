ce3d96a7a3e6df5d441747d4c2a1361b7d6757b5
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-07-28 11:13:17 +0300

    added weather model

---
+++ Weather/Models/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  Weather
+//
+//  Created by Roman Ivanov on 28.07.2022.
+//
+
+import Foundation
+
+struct Weather: Codable {
+    let id: Int
+    let main: String
+    let description: String
+}
