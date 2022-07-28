b787147e886186e81157b6b7aab4ad2c4c18e37f
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-07-28 11:15:00 +0300

    added open weather model

---
+++ Weather/Models/OpenWeather.swift
@@ -0,0 +1,19 @@
+//
+//  OpenWeather.swift
+//  Weather
+//
+//  Created by Roman Ivanov on 28.07.2022.
+//
+
+import Foundation
+
+struct OpenWeather: Codable {
+    let coord: Coordinates
+    let weather: [Weather]
+    let main: WeatherInfo
+    let visibility: Int
+    let wind: Wind
+    let sys: WeatherCountry
+    let name: String
+    let cod: Int
+}
