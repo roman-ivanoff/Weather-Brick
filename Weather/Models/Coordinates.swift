dcc8c3314c58040adf90911204104db91c78d050
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-07-28 11:13:47 +0300

    added coordinates model

---
+++ Weather/Models/Coordinates.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinates.swift
+//  Weather
+//
+//  Created by Roman Ivanov on 28.07.2022.
+//
+
+import Foundation
+
+struct Coordinates: Codable {
+    let lon: Double
+    let lat: Double
+}
