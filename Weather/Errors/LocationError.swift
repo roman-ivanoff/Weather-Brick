3ffc76beb9ac08fb40eac5d61ff203fcf522cf4d
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-08-03 16:23:30 +0300

    changed LocationError

--- Weather/Errors/LocationError.swift
+++ Weather/Errors/LocationError.swift
@@ -7,17 +7,7 @@
 
 import Foundation
 
-struct LocationError: LocationErrorProtocol {
-    var title: String?
-    var code: Int
-    var errorDescription: String? { description }
-    var failureReason: String? { description }
-
-    private var description: String
-
-    init(title: String?, description: String, code: Int) {
-        self.title = title ?? "Location Error"
-        self.description = description
-        self.code = code
-    }
+enum LocationError: Error {
+    case invalidAuthorization
+    case invalidLocationService
 }
