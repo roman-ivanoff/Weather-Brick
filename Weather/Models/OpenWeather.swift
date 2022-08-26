10ef9408233435a51df53ad1c63d81b0aca15d6a
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-08-26 12:41:40 +0300

    added Equatable to the OpenWeather struct

--- Weather/Models/OpenWeather.swift
+++ Weather/Models/OpenWeather.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct OpenWeather: Codable {
+struct OpenWeather: Codable, Equatable {
     let coord: Coordinates
     let weather: [Weather]
     let main: WeatherInfo
@@ -16,4 +16,38 @@ struct OpenWeather: Codable {
     let sys: WeatherCountry
     let name: String
     let cod: Int
+
+    static func == (lhs: OpenWeather, rhs: OpenWeather) -> Bool {
+        var isWeatherEqual = true
+        for lWeather in lhs.weather {
+            for rWeather in rhs.weather {
+                if lWeather.id != rWeather.id {
+                    isWeatherEqual = false
+                    break
+                }
+
+                if lWeather.main != rWeather.main {
+                    isWeatherEqual = false
+                    break
+                }
+
+                if lWeather.description != rWeather.description {
+                    isWeatherEqual = false
+                    break
+                }
+            }
+        }
+
+        return lhs.coord.lat == rhs.coord.lat &&
+        lhs.coord.lon == rhs.coord.lon &&
+        isWeatherEqual &&
+        lhs.main.humidity == rhs.main.humidity &&
+        lhs.main.temp == rhs.main.temp &&
+        lhs.visibility == rhs.visibility &&
+        lhs.wind.deg == rhs.wind.deg &&
+        lhs.wind.speed == rhs.wind.speed &&
+        lhs.sys.country == lhs.sys.country &&
+        lhs.name == rhs.name &&
+        lhs.cod == rhs.cod
+    }
 }
