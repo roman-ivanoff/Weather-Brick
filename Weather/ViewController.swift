7da96a31064778179dfb59fd45b8105da7411208
Roman Ivanov <roman.ivanoff124@gmail.com>
2022-09-11 11:37:24 +0300

    added into viewWillAppear

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -39,6 +39,7 @@ class ViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        weatherBrick.updateWeatherAtCurrentLocation()
     }
 
     private func setInfoView() {
